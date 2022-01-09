efd42f2dbd400df08aca59c8a23866b8de9307d3
Nitin <nsinghboss02@gmail.com>
2022-01-10 00:38:34 +0530

    Initial commit

---
+++ src/com/Recursion/Main.java
@@ -0,0 +1,26 @@
+package com.Recursion;
+public class Main {
+
+    public static void main(String[] args) {
+	// Find minimum and maximum element in an array
+        int[] nums = {8,5,3,4,2,1};
+        sort(nums);
+        int min = nums[0];
+        int max = nums[nums.length-1];
+        System.out.println("The maximum and minimum value in an array are "+max+" , "+min);
+    }
+    public static void sort(int[] nums){
+        for(int i=0;i< nums.length;i++){
+            for(int j=1;j< nums.length-i;j++){
+                if(nums[j]<nums[j-1]){
+                    swap(nums,j,j-1);
+                }
+            }
+        }
+    }
+    public static void swap(int[] nums,int first,int last){
+        int temp = nums[first];
+        nums[first] = nums[last];
+        nums[last] = temp;
+    }
+}
